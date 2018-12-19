6e8ad5ce00deec75db6667a06cf78f58a27bd078
papa.tkach <papa.tkach@gmail.com>
2018-12-19 18:10:13 +0200

    step 1

--- test2.py
+++ test2.py
@@ -1,4 +1,19 @@
 '''
 math in Python
 '''
-print("Hello")
+
+def arabToRoman(number):
+    print(number)
+
+def romanToArab(string):
+    print(string)
+
+print("Convert number in Arab notation to Roman notation")
+arabNumber = int(input("Enter any number in Arab notation: "))
+arabToRoman(arabNumber)
+
+print("Convert number in Roman notation to Arab notation")
+romanStr = str(input("Enter any number in Roman notation: "))
+arabToRoman(romanStr)
+
+
