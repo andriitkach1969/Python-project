62af5467926cb85c2077dbecce5aa60df9c3d2d0
papa.tkach <papa.tkach@gmail.com>
2018-12-21 09:44:08 +0200

    step 2

--- test2.py
+++ test2.py
@@ -1,19 +1,57 @@
-'''
-math in Python
-'''
+ROMANSIGN = [{1: 'I', 4: 'IV', 5: 'V', 9: 'IX'}, {1: 'X', 4: 'XL', 5: 'L', 9: 'XC'},
+             {1: 'C', 4: 'CD', 5: 'D', 9: 'CM'}, {1: 'M'}]
+
+
+def isInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
 
 def arabToRoman(number):
-    print(number)
+    if (not isInt(number)):
+        print("Not a Arab number")
+        return
+    if (number > 0):
+        resultStr = ""
+        print(number)
+        digit = 0
+        i = 0
+        while (True):
+            digit = number % 10
+            number = number // 10
+            print("digit - ", digit)
+            if (digit != 0):
+                tmpStr = ROMANSIGN[i].get(digit, '*')
+                if (tmpStr == '*'):
+                    tmpStr = ''
+                    if (digit in (1, 2, 3)):
+                        base = 1
+                    elif (digit in (6, 7, 8)):
+                        base = 5
+                        digit = digit - base + 1
+                    tmpStr = tmpStr + ROMANSIGN[i].get(base, '*')
+                    for j in range(1, digit):
+                        tmpStr = tmpStr + ROMANSIGN[i].get(1, '*')
+                resultStr = tmpStr + resultStr
+                if (number == 0):
+                    break;
+            i += 1;
+        print("this number in Roman notation is: ", resultStr);
+    else:
+        print("No a valid Arab number")
 
-def romanToArab(string):
-    print(string)
+
+def romanToArab(str):
+    print(str)
+
 
 print("Convert number in Arab notation to Roman notation")
 arabNumber = int(input("Enter any number in Arab notation: "))
 arabToRoman(arabNumber)
 
 print("Convert number in Roman notation to Arab notation")
-romanStr = str(input("Enter any number in Roman notation: "))
-arabToRoman(romanStr)
-
-
+##romanStr = str(input("Enter any number in Roman notation: "))
+##romanToArab(romanStr)
