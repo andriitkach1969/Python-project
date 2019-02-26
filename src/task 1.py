cef850ca05f1d27bd6db8f336d7de9bfbea70855
papa.tkach <papa.tkach@gmail.com>
2019-02-26 17:35:43 +0200

    add

--- src/task 1.py
+++ src/task 1.py
@@ -1,4 +1,5 @@
 import json
+from collections import Iterable
 
 STR_ITEM = 'item'
 STR_NAME = 'name'
@@ -11,6 +12,14 @@ STR_URL = 'url'
 STR_DESCRIPTION = 'description'
 
 
+def enumdata(dictdata, ignore_type=(bytes, str)):
+    for it in dictdata:
+        if isinstance(it, Iterable) and not isinstance(it, ignore_type):
+            yield from enumdata(it)
+        else:
+            yield it
+
+
 def crack(dictdata, listdata):
 
     schemadict = {STR_NAME:'', STR_DESCRIPTION:'', STR_METHOD:'', STR_AUTH:'', STR_HEADER:'', STR_URL:'', STR_BODY:''}
@@ -18,7 +27,7 @@ def crack(dictdata, listdata):
     for item in dictdata[STR_ITEM]:
         gcheck = item.get(STR_REQUEST)
         if gcheck is None:
-            print(item.get('name'))
+            # print(item.get('name'))
             crack(item, listdata)
         else:
             idict = schemadict;
@@ -39,11 +48,18 @@ if __name__ == "__main__":
     rawdata = inFile.read()
     inFile.close()
     jsonData = json.loads(rawdata)
+
     crack(jsonData, resList)
     if len(resList) != 0:
         for i in resList:
             print(i)
     else:
         print('error in parsing')
-
     print(len(resList))
+
+    g = enumdata(jsonData.values())
+    cnt = 0
+    for i in g:
+        print(i)
+        cnt += 1
+    print(cnt)
